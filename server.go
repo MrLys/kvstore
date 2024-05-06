00a05f94f9576553df52b3f1ab61dfac8d272634
Thomas <thomas@ljos.app>
2024-05-06 08:12:21 +0200

    add not working client and server

--- server.go
+++ server.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"sync"
 
 	"github.com/shamaton/msgpack/v2"
 )
+
+type CommandPayload struct {
+	m   string
+	k   string
+	v   string
+	ttl string
+}
+type ResponsePayload struct {
+	c string
+	v string
+}
 
 func main() {
 	listener, err := net.Listen("tcp", "localhost:9696")
@@ -15,20 +28,18 @@ func main() {
 	}
 	defer listener.Close()
 
+	cache := sync.Map{}
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection:", err)
 			continue
 		}
-		go handleConnection(conn)
+		go handleConnection(conn, &cache)
 	}
 
 }
-func handleConnection(conn net.Conn) {
-	type Struct struct {
-		String string
-	}
+func handleConnection(conn net.Conn, cache *sync.Map) {
 	defer conn.Close()
 	buffer := make([]byte, 1024)
 	for {
@@ -37,9 +48,66 @@ func handleConnection(conn net.Conn) {
 			fmt.Println("Could not set up tcp socket:", err)
 			return
 		}
-		resp := Struct{}
-		msgpack.Unmarshal(buffer[:n], &resp)
-		fmt.Printf("Received %s:\n", resp.String)
+		fmt.Println(fmt.Sprintf("Received data: %b", buffer[0]))
+		fmt.Println(fmt.Sprintf("Received data: %b", buffer[1]))
+		fmt.Println(fmt.Sprintf("Received data: %b", buffer[2]))
+		fmt.Println(fmt.Sprintf("Buffer contains %b, %d", buffer, n))
+		cmd, err := marshallCommand(buffer, n)
+		//if err != nil {
+		//	fmt.Println("Failed to marshallCommand", err)
+		//	return
+		//}
+		executeCommand(cmd, conn, cache)
 	}
 
+}
+
+func executeCommand(cmd CommandPayload, conn net.Conn, cache *sync.Map) {
+	if cmd.m == "1" {
+		fmt.Println("Got get command")
+		val, _ := cache.Load(cmd.k)
+		sVal, ok := val.(string)
+		fmt.Println(sVal)
+		if !ok {
+			return
+		}
+		writeResponse(conn, "1", sVal)
+		// get
+	} else if cmd.m == "2" {
+		fmt.Println("Got set command")
+		cache.Store(cmd.k, cmd.v)
+		writeResponse(conn, "1", "")
+		// set
+	} else if cmd.m == "3" {
+		fmt.Println("Get clear command")
+		//clear
+	}
+}
+
+func writeResponse(conn net.Conn, code string, val string) (int, error) {
+	v, err := msgpack.Marshal(ResponsePayload{c: code, v: val})
+	if err != nil {
+		return 1, err
+	}
+	return conn.Write(v)
+}
+
+func marshallCommand(buffer []byte, n int) (cmd CommandPayload, err error) {
+	resp := CommandPayload{}
+	msgpack.Unmarshal(buffer[:n], &resp)
+	fmt.Println(fmt.Sprintf("Command contains %s", resp.m))
+	fmt.Println(fmt.Sprintf("Command contains %s", resp.v))
+	fmt.Println(fmt.Sprintf("Command contains %s", resp.k))
+	fmt.Println(fmt.Sprintf("Command contains %s", resp.ttl))
+	if resp.m == "1" {
+		// get
+		return resp, nil
+	} else if resp.m == "2" {
+		// set
+		return resp, nil
+	} else if resp.m == "3" {
+		//clear
+		return resp, nil
+	}
+	return CommandPayload{}, errors.New("Not a valid command")
 }
--- tcpclient/client.go
+++ tcpclient/client.go
@@ -9,25 +9,74 @@ import (
 	"github.com/shamaton/msgpack/v2"
 )
 
+type CommandPayload struct {
+	m   string
+	k   string
+	v   string
+	ttl string
+}
+type ResponsePayload struct {
+	c string
+	v string
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter greeting:")
-	text, _, _ := reader.ReadLine()
-	type Struct struct {
-		String string
-	}
-	v := Struct{String: fmt.Sprintf("Greeting form client: %v", text)}
-	packedMsg, err := msgpack.Marshal(v)
 	conn, err := net.Dial("tcp", "localhost:9696")
 	if err != nil {
 		fmt.Println("Error connecing to server:", err)
 		return
 	}
 	defer conn.Close()
-	_, err = conn.Write([]byte(packedMsg))
-	if err != nil {
-		fmt.Println("Error connecing to server:", err)
-		return
+	counter := 0
+	for {
+		buffer := make([]byte, 1024)
+		fmt.Println("Enter greeting:")
+		text, _, _ := reader.ReadLine()
+		fmt.Println(fmt.Sprintf("Greeting form client: %s", text))
+		//var m byte
+		//if counter%2 == 0 {
+		//	m = 0x1
+		//} else {
+		//	m = 0x0
+		//}
+		v := CommandPayload{m: "uouauoauaua", k: "test key", v: "test value, what else?", ttl: "0"}
+		packedMsg, err := msgpack.Marshal(v)
+		t := CommandPayload{}
+		err = msgpack.Unmarshal(packedMsg, &t)
+		if err != nil {
+			fmt.Println("Error during Marshal", err)
+		}
+		fmt.Println(fmt.Sprintf("msg : %s", t.k))
+		fmt.Println("packedMsg len", len(packedMsg))
+		for i := 0; i < len(packedMsg); i++ {
+			fmt.Println(fmt.Sprintf("msg : %b", packedMsg[i]))
+		}
+		fmt.Println("Error during Marshal", err)
+		if err != nil {
+			fmt.Println("Error during Marshal", err)
+		}
+		fmt.Println(fmt.Sprintf("packedMsg: %d", packedMsg))
+		_, err = conn.Write([]byte{0x5, 0x0, 0x4})
+		if err != nil {
+			fmt.Println("Error writing to socket", err)
+		}
+
+		counter++
+		if err != nil {
+			fmt.Println("Error connecing to server:", err)
+			return
+		}
+		n, err := conn.Read(buffer)
+		if err != nil {
+			return
+		}
+		resp := ResponsePayload{}
+		msgpack.Unmarshal(buffer[:n], &resp)
+		if resp.c != "1" {
+			fmt.Println("Error during call.")
+		}
+		fmt.Println(resp.v)
 	}
 
 }
